82537d27179c46f5c56f93ed591770204094cff5
Hady Salhab <hadysalhab1@gmail.com>
2020-07-16 15:15:39 +1000

    Refactor error middleware

--- middleware/error.js
+++ middleware/error.js
@@ -8,27 +8,38 @@ const errorHandler = (err, req, res, next) => {
 
 	// Mongoose bad ObjectId
 	if (err.name === "CastError") {
-		const message = `Resource not found`;
-		// convert mongoose exception to custom ErrorResponse obj
-		error = new ErrorResponse(message, 404);
+		error = handleCastErrorDB(error);
 	}
 	// Mongoose duplicate key
-	if (err.code === 1100) {
-		const message = `Duplicate field value entered`;
-		// convert mongoose exception to custom ErrorResponse obj
-		error = new ErrorResponse(message, 400);
+	if (err.code === 11000) {
+		error = handleDuplicateFieldsDB(error);
 	}
 
 	// Mongoose validation error
 	if (err.name === "ValidationError") {
-		const message = Object.values(err.errors).map((el) => el.message);
-		// convert mongoose exception to custom ErrorResponse obj
-		error = new ErrorResponse(message, 400);
+		error = handleValidationErrorDB(error);
 	}
 
 	res.status(error.statusCode || 500).json({
 		success: false,
 		error: error.message || "Server Error",
 	});
+};
+
+// convert mongoose exception to custom ErrorResponse obj
+
+const handleDuplicateFieldsDB = (err) => {
+	const value = err.message.match(/(["'])(\\?.)*?\1/)[0];
+	const message = `Duplicate field value: ${value}. Please use another value!`;
+	return new ErrorResponse(message, 400);
+};
+const handleValidationErrorDB = (err) => {
+	const errors = Object.values(err.errors).map((el) => el.message);
+	const message = `Invalid input data. ${errors.join(". ")}`;
+	return new ErrorResponse(message, 400);
+};
+const handleCastErrorDB = (err) => {
+	const message = `Invalid ${err.path}: ${err.value}.`;
+	return new ErrorResponse(message, 400);
 };
 module.exports = errorHandler;
