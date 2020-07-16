da048be6c2024c79c53ae75b8cff8095956ea187
Hady Salhab <hadysalhab1@gmail.com>
2020-07-16 12:37:32 +1000

    Setup app package structure

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config({
 	path: "./config/config.env",
 });
+const express = require("express");
 const app = express();
+
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/recipes", require("./routes/recipes"));
+
 app.get("/", (req, res, next) => {
 	res.status(200).json({
 		success: true,
