221db2fd72cb21ca8408c0d1e1bf14108a94923b
Hady Salhab <hadysalhab1@gmail.com>
2020-07-18 10:03:23 +1000

    Handle AddRecipeModal form submit

--- client/src/components/recipes/AddRecipeModal.js
+++ client/src/components/recipes/AddRecipeModal.js
@@ -1,14 +1,17 @@
 import React, { useRef, useEffect } from "react";
 import M from "materialize-css/dist/js/materialize.min.js";
-import useForm from "../../hooks/useForm";
+import useFormState from "../../hooks/useForm";
+import { useForm } from "react-hook-form";
 import useIngredients from "../../hooks/useIngredients";
 import { v4 as uuidv4 } from "uuid";
 import { connect } from "react-redux";
 import { addRecipe } from "../../actions";
+
 const AddRecipeModal = (props) => {
-	const [name, onNameChange, resetName] = useForm("");
-	const [direction, onDirectionChange, resetDirection] = useForm("");
-	const [ingredient, onIngredientChange, resetIngredient] = useForm("");
+	const [name, onNameChange, resetName] = useFormState("");
+	const [direction, onDirectionChange, resetDirection] = useFormState("");
+	const [ingredient, onIngredientChange, resetIngredient] = useFormState("");
+	const { clearErrors, trigger, register, errors } = useForm();
 	const {
 		ingredients,
 		addIngredient,
@@ -21,27 +24,33 @@ const AddRecipeModal = (props) => {
 	useEffect(() => {
 		M.Modal.init(modal.current, {
 			onCloseEnd: () => {
-				clearInputs();
+				clearState();
 			},
 		});
 	}, []);
-	const clearInputs = () => {
+
+	const clearState = () => {
 		resetName();
 		resetDirection();
 		resetIngredient();
 		clearIngredients();
+		clearErrors();
 	};
 
-	const handleSubmit = () => {
-		M.Modal.getInstance(modal.current).close();
-		const recipe = {
-			id: uuidv4(),
-			name,
-			direction,
-			ingredients,
-		};
-		props.addRecipe(recipe);
-		clearInputs();
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const result = await trigger();
+		if (result) {
+			M.Modal.getInstance(modal.current).close();
+			const recipe = {
+				id: uuidv4(),
+				name,
+				direction,
+				ingredients,
+			};
+			props.addRecipe(recipe);
+			clearState();
+		}
 	};
 
 	return (
@@ -50,7 +59,7 @@ const AddRecipeModal = (props) => {
 				<h4 className="green-text">Enter Recipe Detail</h4>
 
 				<div className="row">
-					<form className="col s12">
+					<form className="col s12" onSubmit={handleSubmit}>
 						<div className="input-field">
 							<input
 								id="name"
@@ -59,8 +68,14 @@ const AddRecipeModal = (props) => {
 								name="name"
 								value={name}
 								onChange={onNameChange}
+								ref={register({
+									required: true,
+								})}
 							/>
 							<label htmlFor="name">Name</label>
+							{errors.name?.type === "required" && (
+								<span className="helper-text red-text">Please add a name</span>
+							)}
 						</div>
 						<div className="input-field">
 							<textarea
@@ -69,8 +84,22 @@ const AddRecipeModal = (props) => {
 								name="direction"
 								value={direction}
 								onChange={onDirectionChange}
+								ref={register({
+									required: true,
+									minLength: 50,
+								})}
 							></textarea>
 							<label htmlFor="direction">Direction</label>
+							{errors.direction?.type === "required" && (
+								<span className="helper-text red-text">
+									Please add a direction
+								</span>
+							)}
+							{errors.direction?.type === "minLength" && (
+								<span className="helper-text red-text">
+									Direction should be at least 50 characters
+								</span>
+							)}
 						</div>
 
 						<div className="row">
@@ -83,8 +112,18 @@ const AddRecipeModal = (props) => {
 										name="ingredient"
 										value={ingredient}
 										onChange={onIngredientChange}
+										ref={register({
+											validate: () => {
+												return ingredients.length > 0;
+											},
+										})}
 									/>
 									<label htmlFor="ingredient">Ingredient</label>
+									{errors.ingredient?.type === "validate" && (
+										<span className="helper-text red-text">
+											Please add at least 1 ingredient
+										</span>
+									)}
 								</div>
 								<a
 									href="#!"
@@ -99,36 +138,37 @@ const AddRecipeModal = (props) => {
 								</a>
 							</div>
 						</div>
+
+						{ingredients.length > 0 && (
+							<ul className="collection">
+								{ingredients.map((ing) => (
+									<li key={ing} className="collection-item">
+										<div>
+											{ing}
+											<a
+												href="#!"
+												className="secondary-content"
+												onClick={(e) => {
+													e.preventDefault();
+													deleteIngredient(ing);
+												}}
+											>
+												<i className="material-icons green-text">delete</i>
+											</a>
+										</div>
+									</li>
+								))}
+							</ul>
+						)}
+						<div className="right-align">
+							<button
+								type="submit"
+								className="waves-effect waves-light btn green white-text"
+							>
+								Submit
+							</button>
+						</div>
 					</form>
-					{ingredients.length > 0 && (
-						<ul className="collection">
-							{ingredients.map((ing) => (
-								<li key={ing} className="collection-item">
-									<div>
-										{ing}
-										<a
-											href="#!"
-											className="secondary-content"
-											onClick={(e) => {
-												e.preventDefault();
-												deleteIngredient(ing);
-											}}
-										>
-											<i className="material-icons green-text">delete</i>
-										</a>
-									</div>
-								</li>
-							))}
-						</ul>
-					)}
-				</div>
-				<div className="right-align">
-					<button
-						onClick={handleSubmit}
-						className="waves-effect waves-light btn green white-text"
-					>
-						Submit
-					</button>
 				</div>
 			</div>
 		</div>
