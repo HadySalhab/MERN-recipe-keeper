d5746bf01e479a1bbe1473e9f53d1a5d28fead9e
Hady Salhab <hadysalhab1@gmail.com>
2020-07-19 11:01:33 +1000

    Update Redux Authentication

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,67 +1,31 @@
-import {
-	REGISTER_SUCCESS,
-	REGISTER_FAIL,
-	CLEAR_ALERT,
-	AUTH_LOADING,
-	USER_LOADED,
-	AUTH_ERROR,
-	LOGIN_SUCCESS,
-	LOGIN_FAIL,
-	LOGOUT,
-} from "../actions/types";
+import { AUTH_USER, AUTH_LOADING, AUTH_ERROR } from "../actions/types";
 
 const initial = {
-	token: localStorage.getItem("recipe-token"),
-	isAuthenticated: null,
+	authenticated: "",
+	errorMessage: null,
 	loading: false,
-	loadingUser: true,
-	alert: null,
-	user: null,
 };
 
 export default (state = initial, action) => {
 	switch (action.type) {
-		case REGISTER_SUCCESS:
-		case LOGIN_SUCCESS:
+		case AUTH_USER:
 			return {
 				...state,
-				token: action.payload,
-				isAuthenticated: true,
+				authenticated: action.payload,
 				loading: false,
-				loadingUser: false,
+				errorMessage: null,
 			};
-		case REGISTER_FAIL:
-		case LOGIN_FAIL:
 		case AUTH_ERROR:
-		case LOGOUT:
 			return {
 				...state,
-				token: null,
-				isAuthenticated: false,
 				loading: false,
-				loadingUser: false,
-				user: null,
-				alert: action.payload,
-			};
-		case USER_LOADED:
-			return {
-				...state,
-				isAuthenticated: true,
-				loadingUser: false,
-				alert: null,
-				user: action.payload,
-			};
-		case CLEAR_ALERT:
-			return {
-				...state,
-				alert: null,
-				loading: false,
+				errorMessage: action.payload,
 			};
 		case AUTH_LOADING:
 			return {
 				...state,
 				loading: true,
-				alert: null,
+				errorMessage: null,
 			};
 		default:
 			return state;
