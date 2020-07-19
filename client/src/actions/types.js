183c0c04ef860a74e031138277b28b222886f39f
Hady Salhab <hadysalhab1@gmail.com>
2020-07-19 11:21:43 +1000

    Fix AUTH_USER type

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -15,4 +15,4 @@ export const REMOVE_ALERT = "REMOVE_ALERT";
 // Authentication
 export const AUTH_ERROR = "AUTH_ERROR";
 export const AUTH_LOADING = "AUTH_LOADING";
-export const AUTH_USER = "AUTH_LOADING";
+export const AUTH_USER = "AUTH_USER";
