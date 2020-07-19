9f27a6e7282ed3f39c36e6a915560441ee5935b6
Hady Salhab <hadysalhab1@gmail.com>
2020-07-19 13:53:43 +1000

    Prepare for deploy

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const dotenv = require("dotenv");
 dotenv.config({
 	path: "./config/config.env",
 });
-
+const path = require("path");
 const colors = require("colors");
 const express = require("express");
 const connectDB = require("./config/db");
@@ -17,6 +17,15 @@ app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/recipes", require("./routes/recipes"));
 app.use(errorHandler);
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+	// Set static folder
+	app.use(express.static("client/build"));
+	app.get("*", (req, res) =>
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+	);
+}
 
 const PORT = process.env.PORT || 5000;
 
